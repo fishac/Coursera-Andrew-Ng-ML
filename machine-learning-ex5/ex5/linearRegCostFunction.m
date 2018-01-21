e3cdef4f4226ccbcf2bb81ae4d9030e056de10f8
Alex Fish <acfishx@gmail.com>
2018-01-21 09:54:30 -0600

    Finish 100% ex5 week 6

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,15 +20,10 @@ grad = zeros(size(theta));
 %
 
 
+J = (1 / (2 * m)) * sum((X * theta - y) .^ 2) + (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
 
-
-
-
-
-
-
-
-
+grad = (1 / m) * (X' * (X * theta - y));
+grad(2:end) = grad(2:end) + (lambda / m) * theta(2:end);
 
 % =========================================================================
 
